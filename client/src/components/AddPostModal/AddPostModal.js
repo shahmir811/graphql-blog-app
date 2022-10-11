0fc2379bb850063283baad5b40f1f403e9e3256a
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-10-11 09:24:25 +0500

    Added PostCreate Mutation on frontend

--- client/src/components/AddPostModal/AddPostModal.js
+++ client/src/components/AddPostModal/AddPostModal.js
@@ -1,67 +1,95 @@
-import React, { useState } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+import { gql, useMutation } from '@apollo/client';
+
+const CREATE_POST = gql`
+	mutation CreatePost($title: String!, $content: String!) {
+		postCreate(data: { title: $title, content: $content }) {
+			userErrors {
+				message
+			}
+			post {
+				id
+				title
+				content
+				published
+				createdAt
+				user {
+					name
+				}
+			}
+		}
+	}
+`;
 
 export default function AddPostModal() {
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
+	const [createPost, { data, loading }] = useMutation(CREATE_POST);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
+	const [content, setContent] = useState('');
+	const [title, setTitle] = useState('');
+	const [error, setError] = useState(null);
 
-  const handleClick = () => {};
+	useEffect(() => {
+		if (data) {
+			if (data.postCreate.userErrors.length) {
+				setError(data.postCreate.userErrors[0].message);
+			}
+			if (data.postCreate.token) {
+				localStorage.setItem('token', data.postCreate.token);
+			}
+		}
+	}, [data]);
 
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Add Post
-      </Button>
+	const handleClick = () => {
+		if (!content || !title) return;
+		else {
+			createPost({
+				variables: {
+					title,
+					content,
+				},
+			});
+			handleClose();
+		}
+	};
 
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Add Post</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder=""
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </Form.Group>
+	return (
+		<>
+			<Button variant='primary' onClick={handleShow}>
+				Add Post
+			</Button>
 
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Content</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClick}>
-            Add
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+			<Modal show={show} onHide={handleClose} backdrop='static' keyboard={false}>
+				<Modal.Header closeButton>
+					<Modal.Title>Add Post</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group className='mb-3' controlId='formBasicEmail'>
+							<Form.Label>Title</Form.Label>
+							<Form.Control type='text' placeholder='' value={title} onChange={e => setTitle(e.target.value)} />
+						</Form.Group>
+
+						<Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+							<Form.Label>Content</Form.Label>
+							<Form.Control as='textarea' rows={3} value={content} onChange={e => setContent(e.target.value)} />
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					{error && <p>{error}</p>}
+					<Button variant='secondary' onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant='primary' onClick={handleClick}>
+						Add
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
 }
